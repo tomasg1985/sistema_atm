a832217683f1071ab7b7a7e2d5055e73dca42512
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-06-12 11:48:11 -0300

    Se agrego persistencia con sqlite

--- logic.py
+++ logic.py
@@ -1,3 +1,18 @@
+import sqlite3
+from colorama import Fore, Style, init
+init()
+
+conexion = sqlite3.connect("banco.db")
+cursor = conexion.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS banco(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        saldo REAL NOT NULL,
+        contrasena TEXT NOT NULL
+    )
+''')
+
 
 mensaje_exito = "Transaccion exitosa"
 mensaje_error = "Transaccion fallida"
@@ -23,11 +38,21 @@ def extraer_monto(saldo, monto):
         >>> extraer_monto(5000, 6000)
         (5000, "Transaccion fallida", False)
     """
-    
-    if monto > 0 and monto <= saldo:
-        nuevo_saldo = saldo - monto
-        return (nuevo_saldo, mensaje_exito, True)
-    else:
+    try:
+        if monto > 0 and monto <= saldo:
+            nuevo_saldo = saldo - monto
+            
+            cursor.execute('UPDATE banco SET saldo = ?', (nuevo_saldo,))
+            conexion.commit()
+            
+            print(f"{Fore.GREEN}Operacion realizada con éxito" + Style.RESET_ALL)
+            return (nuevo_saldo, mensaje_exito, True)
+        else:
+            return (saldo, mensaje_error, False)
+        
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print(f"{Fore.RED}Error de sistema...{e}" + Style.RESET_ALL)
         return (saldo, mensaje_error, False)
     
 
@@ -52,15 +77,27 @@ def depositar(saldo, monto):
         >>> depositar(5000, -500)
         (5000, "Transaccion fallida", False)
     """
-    
-    if monto > 0:
-        nuevo_saldo = monto + saldo
-        return (nuevo_saldo, mensaje_exito, True)
-    else:
+    try:
+        if monto > 0:
+            nuevo_saldo = monto + saldo
+            
+            cursor.execute('UPDATE banco SET saldo = ?', (nuevo_saldo,))
+            conexion.commit()
+            
+            print(f"{Fore.GREEN}Operacion realizada con éxito" + Style.RESET_ALL)
+            return (nuevo_saldo, mensaje_exito, True)
+        
+        else:
+            print(f"{Fore.RED}Operacion fallida" + Style.RESET_ALL)
+            return (saldo, mensaje_error, False)
+        
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print(f"{Fore.RED}Error al realizar la operacion por fallas del sistema {e}" + Style.RESET_ALL)
         return (saldo, mensaje_error, False)
 
 def cambiar_contrasena(contrasena_actual, nueva_contrasena, confirmacion):
-    
+
     """
     Cambia la contraseña de un usuario tras validar la actual y la coincidencia.
 
@@ -85,14 +122,14 @@ def cambiar_contrasena(contrasena_actual, nueva_contrasena, confirmacion):
         >>> cambiar_contrasena("1234", "1234", "1234")
         (None, "La nueva contraseña debe ser diferente a la actual", False)
     """
-    
+
     if not nueva_contrasena:
         return(None, "La contraseña no puede estar vacía", False)
         
     if nueva_contrasena == contrasena_actual:
         return(None, "La nueva contraseña debe ser diferente a la actual", False)
-    
+
     if nueva_contrasena != confirmacion:
         return(None, "Las contraseñas no coinciden", False)
-    
+
     return(nueva_contrasena, "Contraseña actualizada con éxito", True)
